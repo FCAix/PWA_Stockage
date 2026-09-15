1d876a01c200850a3380070a5889301fda5c64e9
kirisawa0 <younes.m.goldero2004@gmail.com>
2026-09-15 15:08:34 +0200

    PWA_Stockage-1.0.9

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from
   "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const supabaseUrl = "https://jixmmvxqraapeuehnxwz.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeG1tdnhxcmFhcGV1ZWhueHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNzEwMzYsImV4cCI6MjA5OTg0NzAzNn0.CyTK24cXomP52byqS_PaxNQcXjDFlFvdx9DGWqgfiUg";
+const supabaseUrl = "https://zbmtkxqqsxhhiiiuahbj.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpibXRreHFxc3hoaGlpaXVhaGJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMTIwMTIsImV4cCI6MjEwMjc4ODAxMn0.AhrEHW65wJDkPpK4Aa6lkjBjIFoRVeNHWsMk5eO1S7k";
 
 export const supabase = createClient(
   supabaseUrl,
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME =
-    "pwa-stockage-v1.0.8";
+    "pwa-stockage-v1.0.9";
 
 const FILES_TO_CACHE = [
 
